d27a9c8881cdfbb3e6545c7af6e484065458ec08
YShrutiRao <shrutirao914@gmail.com>
2024-04-20 17:24:23 +0530

    files added

---
+++ To-do list/script.js
@@ -0,0 +1,25 @@
+document.addEventListener("DOMContentLoaded", function() {
+    let inputBx = document.querySelector('#inputBx');
+    let list = document.querySelector('#list');
+
+    inputBx.addEventListener("keyup", function(event){
+        if (event.key == "Enter") {
+            addItem(this.value);
+            this.value = "";
+        }
+    });
+
+    let addItem = (inputBxValue) => {
+        let listItem = document.createElement("li");
+        listItem.innerHTML = `${inputBxValue}<i></i>`;
+
+        listItem.addEventListener("click", function(){
+            this.classList.toggle('done');
+        })
+        listItem.querySelector("i").addEventListener("click",
+    function(){
+        listItem.remove();
+    })
+        list.appendChild(listItem);
+    };
+});
